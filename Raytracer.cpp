a8fff5388b51671187bda972b0db4c372b8918e4
MohamedSamy <Mohamed.Samy.K@gmail.com>
2016-12-14 11:10:00 +0200

    Refactoring, Adding Vect class

--- Raytracer.cpp
+++ Raytracer.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 
+#include <Vect.h>
+
 #define OBJS_NUM 4
 
 using namespace std;
@@ -17,6 +19,8 @@ int main()
     double f = 256;
     double proj_x = 3;
     double e_x = height, e_y = 80, e_z = height;
+    Vect eye_pos = Vect(height, 80, height);
+
     int p1_r = 128, p1_g = 0, p1_b = 0;
     int p2_r = 0, p2_g = 128, p2_b = 0;
     int p3_r = 0, p3_g = 0, p3_b = 128;
@@ -24,13 +28,22 @@ int main()
     // plane normal -- normalized
     double angle = 45;
     double n_x = cos(M_PI * angle / 180), n_y = 0, n_z = sin(M_PI * angle / 180), D = -f * sin(M_PI * angle / 180);
+    Vect projection_normal = Vect(n_x, n_y, n_z);
     //double pn3_x = cos(M_PI * angle / 180), pn3_y = 0, pn3_z = -sin(M_PI * angle / 180), p3_D = 35 * sin(M_PI * angle / 180);
     //double pn2_x = -cos(M_PI * angle / 180), pn2_y = 0, pn2_z = -sin(M_PI * angle / 180), p2_D = 35 * sin(M_PI * angle / 180);
-    double pn3_x = 0, pn3_y = 0, pn3_z = 1, p3_D = 0;
-    double pn2_x = 1, pn2_y = 0, pn2_z = 0, p2_D = 0;
-    double pn1_x = 0, pn1_y = 1, pn1_z = 0, p1_D = 0;
+//    double pn3_x = 0, pn3_y = 0, pn3_z = 1, p3_D = 0;
+    Vect pn3 = Vect(0, 0, 1);
+    double p3_D = 0;
+//    double pn2_x = 1, pn2_y = 0, pn2_z = 0,
+    Vect pn2 = Vect(1, 0, 0);
+    double p2_D = 0;
+//    double pn1_x = 0, pn1_y = 1, pn1_z = 0, p1_D = 0;
+    Vect pn1 = Vect(0, 1, 0);
+    double p1_D = 0;
 
-    double sc1_x = 30, sc1_y = 81, sc1_z = 30, s1_r = 50;
+//    double sc1_x = 30, sc1_y = 81, sc1_z = 30, s1_r = 50;
+    Vect sc1 = Vect(30, 81, 30);
+    double s1_r = 50;
 
     int*** arr = new int**[width];
     for (int i = 0; i < width; i++)
@@ -44,52 +57,44 @@ int main()
         {
 
             double x = (((j - 0)*(f - proj_x - proj_x)) / (height - 1)) + proj_x;
-
 //            double y = height*0.5-i;
             double y = (((i - 0)*(f - proj_x - proj_x)) / (height - 1)) + proj_x;
-
-            double dd_x = x - e_x, dd_y = y - e_y;
             double z = -1*(x*n_x + y*n_y + D)/n_z;
-            double dd_z = z - e_z;
-            double norm = sqrt(dd_x*dd_x + dd_y*dd_y + dd_z*dd_z);
-            double d_x = dd_x / norm;
-            double d_y = dd_y / norm;
-            double d_z = dd_z / norm;
 
-            cout << y << endl;
+            Vect ray_direction = Vect(x - e_x, y - e_y, z - e_z).normalize();
 
             double object_ts[OBJS_NUM];
             double object_denoms[OBJS_NUM];
 
             // planes intersection
             double t1;
-            double denom1 = (pn1_x*d_x + pn1_y*d_y + pn1_z*d_z);
+
+            double denom1 = pn1.dotProduct(ray_direction);
             if (abs(denom1) - 1e-5 <= 0)
                 t1 = 1e7;
-            t1 = -(p1_D + (pn1_x*e_x + pn1_y*e_y + pn1_z*e_z)) / denom1;
+            t1 = -(p1_D + pn1.dotProduct(eye_pos)) / denom1;
             object_ts[0] = t1;
             object_denoms[0] = denom1;
             double t2;
-            double denom2 = (pn2_x*d_x + pn2_y*d_y + pn2_z*d_z);
+            double denom2 = pn2.dotProduct(ray_direction);
             if (abs(denom2) - 1e-5 <= 0)
                 t2 = 1e7;
-            t2 = -(p2_D + (pn2_x*e_x + pn2_y*e_y + pn2_z*e_z)) / denom2;
+            t2 = -(p2_D + pn2.dotProduct(eye_pos)) / denom2;
             object_ts[1] = t2;
             object_denoms[1] = denom2;
             double t3;
-            double denom3 = (pn3_x*d_x + pn3_y*d_y + pn3_z*d_z);
+            double denom3 = pn3.dotProduct(ray_direction);
             if (abs(denom3) - 1e-5 <= 0)
                 t3 = 1e7;
-            t3 = -(p3_D + (pn3_x*e_x + pn3_y*e_y + pn3_z*e_z)) / denom3;
+            t3 = -(p3_D + pn3.dotProduct(eye_pos)) / denom3;
             object_ts[2] = t3;
             object_denoms[2] = denom3;
 
             // Sphere intersection
             // Right Sphere
-            //
-            double C = ( (e_x-sc1_x)*(e_x-sc1_x) + (e_y-sc1_y)*(e_y-sc1_y) + (e_z-sc1_z)*(e_z-sc1_z) ) - s1_r*s1_r;
-            double B = 2 * (d_x*(e_x-sc1_x) + d_y*(e_y-sc1_y) + d_z*(e_z-sc1_z));
-            double A = d_x*d_x + d_y*d_y + d_z*d_z;
+            double C =  ((eye_pos-sc1).squaredMagnitude()) - s1_r*s1_r;
+            double B = ray_direction.dotProduct( eye_pos-sc1 ) * 2;
+            double A = ray_direction.squaredMagnitude();
             double D = B*B - 4 * A * C;
             double t4, t4_1, t4_2;
             if ( D < 0 ){
@@ -97,11 +102,9 @@ int main()
                 t4 = 1e7;
             } else if ( abs(D) < 1e-5 ) {
                 // One intersection
-                // cout << "1" << endl;
                 t4 = (-1*B) / (2*A);
             } else if ( D > 0 ) {
                 // Two intersections
-                // cout << "2" << endl;
                 t4_1 = (-1*B + sqrt(D)) / (2*A);
                 t4_2 = (-1*B - sqrt(D)) / (2*A);
                 if( t4_1 < t4_2 )
@@ -110,16 +113,9 @@ int main()
                     t4 = t4_2;
             }
             if( t4 > 0 && t4 < 1e7 ){
-                double s_intersect_x = e_x + t4*d_x;
-                double s_intersect_y = e_y + t4*d_y;
-                double s_intersect_z = e_z + t4*d_z;
-
-                double sn1_x = s_intersect_x - sc1_x, sn1_y = s_intersect_y - sc1_y, sn1_z = s_intersect_z - sc1_z;
-                double norm = sqrt(sn1_x*sn1_x + sn1_y*sn1_y + sn1_z*sn1_z);
-                sn1_x /= norm;
-                sn1_y /= norm;
-                sn1_z /= norm;
-                double denom4 = (sn1_x*d_x + sn1_y*d_y + sn1_z*d_z);
+                Vect s_intersect = eye_pos + ray_direction*t4;
+                Vect sn1 = (s_intersect - sc1).normalize();
+                double denom4 = sn1.dotProduct(ray_direction);
                 object_denoms[3] = denom4;
             }
             object_ts[3] = t4;
@@ -147,19 +143,17 @@ int main()
                     intersected_any = true;
                     denom = object_denoms[object_t_idx];
                     t = object_ts[object_t_idx];
-                    double intersect_x = e_x + t*d_x;
-                    double intersect_y = e_y + t*d_y;
-                    double intersect_z = e_z + t*d_z;
+                    Vect intersect = eye_pos + ray_direction*t;
                     double intersect_1, intersect_2;
                     if( object_t_idx == 0 ){
-                        intersect_1 = intersect_x;
-                        intersect_2 = intersect_z;
+                        intersect_1 = intersect.getVX();
+                        intersect_2 = intersect.getVZ();
                     } else if( object_t_idx == 1 ){
-                        intersect_1 = intersect_z;
-                        intersect_2 = intersect_y/ height * f;
+                        intersect_1 = intersect.getVZ();
+                        intersect_2 = intersect.getVY()/ height * f;
                     } else if( object_t_idx == 2 ){
-                        intersect_1 = intersect_x;
-                        intersect_2 = intersect_y/ height * f;
+                        intersect_1 = intersect.getVX();
+                        intersect_2 = intersect.getVY()/ height * f;
                     }
                     // plane stripes
                     if( object_t_idx < 3 ){
---
+++ Vect.h
@@ -0,0 +1,79 @@
+#ifndef Vect_H
+#define Vect_H
+
+#include <math.h>
+
+class Vect {
+    double x, y, z;
+
+    public:
+
+    Vect();
+
+    Vect(double, double, double);
+
+    // method functions
+
+    double getVX() { return x; }
+    double getVY() { return y; }
+    double getVZ() { return z; }
+
+    double magnitude() { return sqrt(x*x + y*y +z*z); }
+
+    double squaredMagnitude() { return (x*x + y*y +z*z); }
+
+    Vect normalize() {
+        double Vmag = this->magnitude();
+        return Vect(x/ Vmag, y/ Vmag, z/ Vmag);
+    }
+
+    Vect negative () {
+        return Vect (-x, -y, -z);
+    }
+
+    double dotProduct(Vect v) {
+        return x*v.getVX() + y*v.getVY() + z*v.getVZ();
+    }
+
+    Vect crossProduct(Vect v) {
+        return Vect (y*v.getVZ() - z*v.getVY(), z*v.getVX() - x*v.getVZ(), x*v.getVY() - y*v.getVX());
+    }
+
+    Vect vectAdd (Vect v) {
+        return Vect (x + v.getVX(), y + v.getVY(), z + v.getVZ());
+    }
+
+    Vect vectMult (double scalar) {
+        return Vect (x*scalar, y*scalar, z*scalar);
+    }
+
+    Vect operator+(Vect v_1){
+        return Vect (x + v_1.getVX(), y + v_1.getVY(), z + v_1.getVZ());
+    }
+
+//    Vect operator=(Vect){
+//        return Vect (-x, -y, -z);
+//    }
+
+    Vect operator-(Vect v_1){
+        return Vect (x - v_1.getVX(), y - v_1.getVY(), z - v_1.getVZ());
+    }
+
+    Vect operator*(double scalar){
+        return Vect (x*scalar, y*scalar, z*scalar);
+    }
+
+};
+
+Vect::Vect () {
+    x = 0;
+    y = 0;
+    z = 0;
+}
+
+Vect::Vect (double i, double j, double k) {
+    x = i;
+    y = j;
+    z = k;
+}
+#endif // Vect_H
