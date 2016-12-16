db3c382e7d4b5733f98a66a0cf71c19ced9c8f0e
keissa <kareem.eissa@outlook.com>
2016-12-16 13:31:46 +0200

    phase 3

--- Raytracer.cpp
+++ Raytracer.cpp
@@ -14,11 +14,17 @@ void writePPM(const char * imagepath, int width, int height, int*** img, int col
 bool intersect_plane(Vect pos, Vect ray_direction, Vect plane_normal, double D, two_doubles& td);
 bool intersect_sphere(Vect pos, Vect ray_direction, Vect center, double radius, two_doubles& td);
 
+bool is_shadow(Vect start_p, Vect ray_direction, double intersect_dist);
+three_doubles compute_shade(Vect start_p, Vect ray_direction, int depth);
+
 int width = 256 * 2, height = 256 * 2;
 double freq = 200;
 double thickness = 0.2;
 double f = 256;
 double proj_x = 3;
+
+int global_depth = 4;
+double epsilone = 0.01;
 
 Vect O(0, 0, 0);
 Vect X(1, 0, 0);
@@ -31,6 +37,25 @@ Vect look_at = Vect(0, 0.5, 0);
 Vect cam_dir = (look_at - eye_pos).normalize();
 Vect cam_right = Y.crossProduct(cam_dir).normalize();
 Vect cam_down = (cam_dir.crossProduct(cam_right).normalize())*(-1);
+
+int p1_r = 128, p1_g = 0, p1_b = 0;
+int p2_r = 0, p2_g = 128, p2_b = 0;
+int p3_r = 0, p3_g = 0, p3_b = 128;
+int kd = 2, ka = 1;
+int ks = 100;
+double spec_gamma = 8;
+
+float kref[] = {0.0, 0.0, 0.0, 1.0, 1.0, 1.0};
+// plane normals -- normalized
+Vect pn[] = { Vect(0,1,0), Vect(1,0,0), Vect(0,0,1) };
+double p_D[] = { 0, 0, 0 };
+
+// Spheres
+double s_r[] = { 0.4, 0.4, 0.4 };
+Vect sc[] = { Vect(3.0, s_r[0] + 0.01, 1.5), Vect(2.5, s_r[1] + 0.01, 2.5), Vect(1.5, s_r[2] + 0.01, 3.0) };
+
+// Lights
+Vect light1_pos = Vect(3, s_r[1] + 1, 3);
 
 double max(double x, double y)
 {
@@ -69,23 +94,6 @@ two_doubles max(double x, double y, double z)
 
 int main()
 {
-	int p1_r = 128, p1_g = 0, p1_b = 0;
-	int p2_r = 0, p2_g = 128, p2_b = 0;
-	int p3_r = 0, p3_g = 0, p3_b = 128;
-	int kd = 5, ka = 1;
-    int ks = 200;
-	double gamma = 8;
-	// plane normals -- normalized
-	Vect pn[] = { Vect(0,1,0), Vect(1,0,0), Vect(0,0,1) };
-	double p_D[] = { 0, 0, 0 };
-
-	// Spheres
-	double s_r[] = { 0.4, 0.4, 0.4 };
-	Vect sc[] = { Vect(3.0, s_r[0] + 0.01, 1.5), Vect(2.5, s_r[1] + 0.01, 2.5), Vect(1.5, s_r[2] + 0.01, 3.0) };
-
-	// Lights
-	Vect light1_pos = Vect(3, s_r[1] + 1, 3);
-
 	Vect normal;
 
 	int*** arr = new int**[width];
@@ -102,7 +110,13 @@ int main()
 			double jamnt = ((height - j) + 0.5) / height;
 			Vect ray_direction = (cam_dir + cam_right*(jamnt - 0.5) +
 				cam_down*(iamnt - 0.5)).normalize();
-			double object_ts[OBJS_NUM];
+				
+			three_doubles color = compute_shade(eye_pos, ray_direction, 0);
+			arr[i][j][0] = (int)color.r;
+			arr[i][j][1] = (int)color.g;
+			arr[i][j][2] = (int)color.b;
+				
+			/*double object_ts[OBJS_NUM];
 			double object_denoms[OBJS_NUM];
 			two_doubles t_d;
 			// planes intersection
@@ -273,7 +287,7 @@ int main()
                     ref1_db = kd*max(0.05, second_normal.dotProduct(ref1_intersect2light))*p_b_2 / 10 / ref1_intersect_dist;
                     Vect halfway2 = (sec_ref1_direction*(-1) + ref1_intersect2light).normalize();
                     double specular2 = halfway2.dotProduct(second_normal);
-                    ref1_s = ks*pow(specular2, gamma);
+                    ref1_s = ks*pow(specular2, spec_gamma);
                     break;
                 }
 
@@ -287,39 +301,195 @@ int main()
 				//cout << "shade\n";
 				arr[i][j][0] = ka 
 					+ kd*max(0.05, normal.dotProduct(intersect2light))*p_r / 10 / intersect_dist
-                    + ks*pow(specular, gamma)
+                    + ks*pow(specular, spec_gamma)
                     + ref1_coeff*(ref1_a + ref1_dr + ref1_s);
 				arr[i][j][1] = ka 
 					+ kd*max(0.05, normal.dotProduct(intersect2light))*p_g / 10 / intersect_dist
-                    + ks*pow(specular, gamma)
+                    + ks*pow(specular, spec_gamma)
                     + ref1_coeff*(ref1_a + ref1_dg + ref1_s);
 				arr[i][j][2] = ka 
 					+ kd*max(0.05, normal.dotProduct(intersect2light))*p_b / 10 / intersect_dist
-                    + ks*pow(specular, gamma)
+                    + ks*pow(specular, spec_gamma)
                     + ref1_coeff*(ref1_a + ref1_db + ref1_s);
 			}
 			else
 			{
 				arr[i][j][0] = ka
 					+ kd*max(0.05, normal.dotProduct(intersect2light))*p_r / intersect_dist / intersect_dist
-                    + ks*pow(specular, gamma)
+                    + ks*pow(specular, spec_gamma)
                     + ref1_coeff*(ref1_a + ref1_dr + ref1_s);
 				arr[i][j][1] = ka 
 					+ kd*max(0.05, normal.dotProduct(intersect2light))*p_g / intersect_dist / intersect_dist
-                    + ks*pow(specular, gamma)
+                    + ks*pow(specular, spec_gamma)
                     + ref1_coeff*(ref1_a + ref1_dg + ref1_s);
 				arr[i][j][2] = ka 
 					+ kd*max(0.05, normal.dotProduct(intersect2light))*p_b / intersect_dist / intersect_dist
-                    + ks*pow(specular, gamma)
+                    + ks*pow(specular, spec_gamma)
                     + ref1_coeff*(ref1_a + ref1_db + ref1_s);
 			}
 			// clip values
 			arr[i][j][0] = arr[i][j][0] > 255 ? 255 : arr[i][j][0];
 			arr[i][j][1] = arr[i][j][1] > 255 ? 255 : arr[i][j][1];
-			arr[i][j][2] = arr[i][j][2] > 255 ? 255 : arr[i][j][2];
+			arr[i][j][2] = arr[i][j][2] > 255 ? 255 : arr[i][j][2];*/
 		}
 	writePPM("test.ppm", width, height, arr);
 	return 0;
+}
+
+three_doubles compute_shade(Vect start_p, Vect ray_direction, int depth)
+{
+    three_doubles color;
+    color.r = 0;
+    color.g = 0;
+    color.b = 0;
+    if(depth == global_depth)
+        return color;
+
+    //shading
+	double object_ts[OBJS_NUM];
+	double object_denoms[OBJS_NUM];
+	two_doubles t_d;
+	// planes intersection
+	for (int pl = 0; pl < 3; pl++)
+	{
+		intersect_plane(start_p, ray_direction, pn[pl], p_D[pl], t_d);
+		object_ts[pl] = t_d.t;
+		object_denoms[pl] = t_d.denom;
+	}
+
+	// Sphere intersections
+	for (int sp = 0; sp < 3; sp++)
+	{
+		intersect_sphere(start_p, ray_direction, sc[sp], s_r[sp], t_d);
+		object_denoms[sp + 3] = t_d.denom;
+		object_ts[sp + 3] = t_d.t;
+	}
+
+	double denom = 0;
+
+    double min_t = 10000000;
+    int min_ind = -1;
+    
+    Vect normal;
+
+    // Check shortest primary intersection
+	for (int object_t_idx = 0; object_t_idx < OBJS_NUM; object_t_idx++) {
+		double object_t = object_ts[object_t_idx];
+		if (object_t > epsilone && object_t < 1e7 && object_t < min_t) {
+			min_t = object_t;
+			min_ind = object_t_idx;
+		}
+	}
+	if(min_ind == -1)
+	{
+	    return color;
+	}
+	
+	denom = object_denoms[min_ind];
+	//t = object_ts[min_ind];
+	if (min_ind < 3)
+		normal = pn[min_ind];
+	else
+		normal = (start_p + ray_direction * min_t - sc[min_ind-3]).normalize();
+	Vect intersect = start_p + ray_direction * min_t;
+	double intersect_1, intersect_2;
+	t_d = max(intersect.getVX(), intersect.getVY() / height * f * 2, intersect.getVZ());
+	intersect_1 = t_d.denom;
+	intersect_2 = t_d.t;
+	if (min_ind < 3)
+		// plane stripes
+		color.r = color.g = color.b = 255 * (abs(cos(freq / f * intersect_1 * 2 * M_PI)) > thickness 
+			&& abs(cos(freq / f * intersect_2 * 2 * M_PI)) > thickness);
+	else {
+		// spheres
+		color.r = 255 * (min_ind == 3||min_ind == 4);
+		color.g = 255 * (min_ind == 4);
+		color.b = 255 * (min_ind == 5||min_ind == 4);
+		//color.r = color.g = color.b = 255;
+	}
+
+	Vect intersect2light = light1_pos - intersect;
+	double intersect_dist = intersect2light.magnitude();
+	intersect2light = intersect2light.normalize();
+	bool shadow = is_shadow(intersect, intersect2light, intersect_dist);
+
+	Vect halfway = (ray_direction*(-1) + intersect2light).normalize();
+	double specular = halfway.dotProduct(normal);
+	
+	three_doubles reflected_color;
+	reflected_color.r = 0;
+    reflected_color.g = 0;
+    reflected_color.b = 0;
+    
+	if(kref[min_ind] > 0)
+	{
+        Vect ref_ray_direction = ray_direction - normal * (ray_direction.dotProduct(normal)) * 2;
+        ref_ray_direction = ref_ray_direction.normalize();
+	    reflected_color = compute_shade(intersect, ref_ray_direction, 1);
+	    /*reflected_color.r /= 5;
+        reflected_color.g /= 5;
+        reflected_color.b /= 5;*/
+    }
+
+	if (shadow || depth > 0)
+	{
+		//cout << "shade\n";
+		color.r = ka 
+			+ kd*max(0.05, normal.dotProduct(intersect2light))*color.r / 10 / intersect_dist
+            + ks*pow(specular, spec_gamma)
+            + kref[min_ind] * reflected_color.r;
+            
+		color.g = ka 
+			+ kd*max(0.05, normal.dotProduct(intersect2light))*color.g / 10 / intersect_dist
+            + ks*pow(specular, spec_gamma)
+            + kref[min_ind] * reflected_color.g;
+            
+		color.b = ka 
+			+ kd*max(0.05, normal.dotProduct(intersect2light))*color.b / 10 / intersect_dist
+            + ks*pow(specular, spec_gamma)
+            + kref[min_ind] * reflected_color.b;
+	}
+	else
+	{
+		color.r = ka
+			+ kd*max(0.05, normal.dotProduct(intersect2light))*color.r / intersect_dist / intersect_dist
+            + ks*pow(specular, spec_gamma)
+            + kref[min_ind] * reflected_color.r;
+            
+		color.g = ka 
+			+ kd*max(0.05, normal.dotProduct(intersect2light))*color.g / intersect_dist / intersect_dist
+            + ks*pow(specular, spec_gamma)
+            + kref[min_ind] * reflected_color.g;
+            
+		color.b = ka 
+			+ kd*max(0.05, normal.dotProduct(intersect2light))*color.b / intersect_dist / intersect_dist
+            + ks*pow(specular, spec_gamma)
+            + kref[min_ind] * reflected_color.b;
+	}
+
+    //color clipping    
+    color.r = color.r < 0? 0: color.r;
+    color.g = color.g < 0? 0: color.g;
+    color.b = color.b < 0? 0: color.b;  
+
+    color.r = color.r > 255? 255: color.r;
+    color.g = color.g > 255? 255: color.g;
+    color.b = color.b > 255? 255: color.b;  
+    
+    return color;
+}
+
+bool is_shadow(Vect start_p, Vect ray_direction, double intersect_dist)
+{
+	two_doubles t_d;
+	bool shade = false;
+	for(int pl=0; pl<3; pl++)
+		if (!shade && intersect_plane(start_p, ray_direction, pn[pl], p_D[pl], t_d) && t_d.t < intersect_dist)
+			shade = true;
+	for (int sp = 0; sp<3; sp++)
+		if (!shade && intersect_sphere(start_p, ray_direction, sc[sp], s_r[sp], t_d) && t_d.t < intersect_dist)
+			shade = true;
+	return shade;
 }
 
 void writePPM(const char * imagepath, int width, int height, int*** img, int color)
